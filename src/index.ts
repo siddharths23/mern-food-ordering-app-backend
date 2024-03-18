ef3a02ce471bcb34d16a1db5c7b02eb1cc599c15
Siddharth Sankar <siddharthsankar23@gmail.com>
2024-03-17 23:31:18 -0700

    adding profile features

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("connected to database"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 //creates new express server
 const app = express();
@@ -18,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 // app.get("/test", async (req: Request, res: Response) => {
 //   res.json({ message: "Hello!" }); // returns json res when gets req
